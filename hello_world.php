88937e4e12240ce3e82ba43aa090c80ed832ff85
AndresArvato <andres.krueger@arvato.com>
2016-03-30 16:28:47 +0200

    fertig

--- hello_world.php
+++ hello_world.php
@@ -1,42 +1,38 @@
+<?php
+# Farbe erwuerfeln
+	$lookUp = array(10 => 'A', 11 => 'B', 12 => 'C', 13 => 'D', 14 => 'E', 15 => 'F');
+  	$color = array("#", rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15));
+  	
+# Array ausgeben
+	echo "Urspruenglicher Farbcode: <br>";
+	for ($zaehler=1; $zaehler < 7; $zaehler++) {
+		if ($color[$zaehler] >= 10) {
+			$color[$zaehler] = $lookUp[ $color[$zaehler] ];
+		}
+  	}
+	
+# Umwandeln der Zahlen 10,11,12,13,14,15 in Buchstaben
+	
+
+
+# Array in String
+	$colorStr = implode ('', $color );
+  	
+# Farbe ausgegeben
+#	echo "Farbcode <br>";
+#	for ($zaehler=0; $zaehler < count($color); $zaehler++) {
+# 	echo $color[$zaehler]. "|"; }
+
+?>
 <!doctype html> 
 <html>
 <head> 
-	<title>Untergrundfarbwechsel</title>
+	<title>Hello World</title>
 </head>
+<body style = "background-color: <?php echo $colorStr;?>" >
 
-<body>
-<?php
-# Farbe erwuerfeln
-  	$color = array(rand(0, 15),rand(0, 15),rand(0, 15),rand(0, 15));
-  	
-# Array ausgeben
-	echo "Urspruenglicher Farbcode <br>";
-	for ($zaehler=0; $zaehler < count($color); $zaehler++) {
- 	echo $color[$zaehler]. "|"; }
-  	
-# Umwandeln der Zahlen 10,11,12,13,14,15 in Buchstaben
-	
-  	if ($color == 10) {
-	$color = 'A';
-  	} else if ($color == 11) {
-	$color = 'B';
-  	} else if ($color == 12) {
-	$color = 'C';
-  	} else if ($color == 13) {
-	$color = 'D';
-  	} else if ($color == 14) {
-	$color = 'E';
-  	} else if ($color == 15) {
-	$color = 'F';
-  	} else {
-  	}
-  	
-# Farbe ausgeben
-	echo "Farbcode <br>";
-	for ($zaehler=0; $zaehler < count($color); $zaehler++) {
- 	echo $color[$zaehler]. "|"; }
- 	bgcolor= $color
-?>
+<h1><?php echo $colorStr;?></h1>
+
 </body>
 </html>
 
