b435a87da5b914a798f2106baaa8552d3d766772
AndresArvato <andres.krueger@arvato.com>
2016-04-07 11:41:05 +0200

    aenderung

--- hello_world.php
+++ hello_world.php
@@ -1,5 +1,5 @@
 <?php
-# Farbe erwuerfeln
+# Farbe  erwuerfeln
 	$lookUp = array(10 => 'A', 11 => 'B', 12 => 'C', 13 => 'D', 14 => 'E', 15 => 'F');
   	$color = array("#", rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15), rand(0, 15));
   	
