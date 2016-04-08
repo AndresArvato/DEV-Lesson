5b1d89fa785cd4cbcb7cdf73dd5a010d82351527
AndresArvato <andres.krueger@arvato.com>
2016-04-08 12:53:15 +0200

    Neue Version

--- Funktionsreferenz/rand.php
+++ Funktionsreferenz/rand.php
@@ -1,27 +1,12 @@
 <?php
 
-# Array definieren und zugreifen
+# rand
 
-$wochentage = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag");
-echo $wochentage[1];
+$zufall = rand (0,100)
+echo $zufall;
 
-# wochentage hat nun 7 Werte (Montag-Sontag). Über wochentage[1] wird auf das zweite Array zugegriffen, da mit 0 begonnen wird.
+# Es wird "zufällig" ein Wert von 0 bis 100 gewählt und ausgegeben. 
 
-# Assoziative Array definieren und zugreifen
-
-$wochentage = array("mo" => "Montag","di" => "Dienstag","mi" => "Mittwoch","do" => "Donnerstag","fr" => "Freitag","sa" => "Samstag","so" => "Sonntag");
-echo $wochentage["mo"];
-
-# wochentage hat nun 7 Werte (Montag-Sontag). Über wochentage["mo"] wird auf das erste Array zugegriffen.
-
-# Weitere Werte einem Arry hinzugefügt
-
-$wochentage = array("Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag");
-$wochentage[] = "Montag";
-
-echo $wochentage[6];
-
-# Der Montag wird nachträglich dem Array hinzugefügt und wird hinten angefügt. Daher ist der Tag dann über wochentage[6] aufrufbar, da mit 0 begonnen wird. 
 
 ?>
 
---
+++ formular.php
@@ -0,0 +1,19 @@
+<?php
+
+print_r($_REQUEST);
+
+?>
+<!doctype html> 
+<html>
+<head> 
+	<title>Formular</title>
+</head>
+<body>
+
+<form method="post">
+    <p>Gib deinen Namen ein: <input type="text" name="benutzername" /></p>
+    <input type="submit" value="absenden" name="submitBtn" />
+</form>
+
+</body>
+</html>i
